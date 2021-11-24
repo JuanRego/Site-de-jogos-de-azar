0b9a28e7abe726995f2f3c58cfa01191a6cc353d
Juan Rego <juanrego.s@outlook.com>
2021-11-24 18:33:52 -0300

    show the animals and the rules on the menu

--- jogo-do-bicho/script.js
+++ jogo-do-bicho/script.js
@@ -1,18 +1,33 @@
+const call = document.getElementById("call-jogo-do-bicho") /*chama o botão que escolhe o jogo do bicho*/
 const game = document.getElementById("game") /*puxa onde vai ficar o jogo*/
 const gameInterface = document.getElementById("gameInterface") /*puxa toda interface visivel do jogo*/
-const animals = document.getElementById("animalsPanel") /*puxa os paneis dos animais*/
+const animals = document.getElementById("animalsPanel") /*puxa os paineis dos animais*/
 const betButton = document.getElementById("betButton") /*puxa o botão de apostas*/
 const totalCoinsWay = document.getElementById("totalCoins") /*puxa o display de total de moedas*/
 const historicoButton = document.getElementById("historicoButton") /*puxa o botão do historico e regras*/
-const historicoList = document.getElementById("historicoList") /*puxa o display do historico*/
+const menu = document.getElementById("menu") /*puxa o display do menu*/
+const historicoContent = document.getElementById("historicoContent") /*puxa a lista onde vai imprimir os animais do historico*/
+const showSortedAnimal = document.getElementById("show-sorted-animal") /*puxa o placar onde mostra o animal sorteado*/
 const historico = [];
+const animais =[
+ ["Avestruz", "Águia", "Burro", "Borboleta", "Cachorro"]
+, ["Cabra", "Carneiro", "Camelo", "Cobra", "Coelho"]
+, ["Cavalo", "Elefante", "Galo", "Gato", "Jacaré"]
+, ["Leão", "Macaco", "Porco", "Pavão", "Peru"]
+, ["Touro", "Tigre", "Urso", "Veado", "Vaca"]
+]
+
+const animalssvg = [
+    ["./jogo-do-bicho/img/aguia", ""]
+]
+
 var column = 0;
 var line = 0;
 var coins = 0;
 var totalCoins = 1000;
+var animal = "";
 
 /*Mapa das tiles dos animais*/
-
 const map = [
     ["", "", "", "", ""],
     ["", "", "", "", ""],
@@ -26,7 +41,8 @@ const animalSelect = (e) => {
     console.log(e.target);
     target = e.target
     column = Number(target.getAttribute ("column"));
-    line = Number(target.getAttribute ("line"));   
+    line = Number(target.getAttribute ("line"));
+    animal = target.getAttribute("animal");   
 }
 
 /*ouve o botão de selecionar o animal*/
@@ -39,7 +55,7 @@ const handleBuildLine = (currentLine, line) =>{
 
         div.setAttribute("column", column)
         div.setAttribute("line", line)
-
+        div.setAttribute("animal", animais[line][column])
         div.innerText = currentValue
 
         animals.appendChild(div);
@@ -55,20 +71,26 @@ const handleBuildMap = () => {
 
 handleBuildMap();
 
-const betAction = (e) => {
-    coins = document.getElementById("coins").value;
+function betAction(e) {
+    coins = document.getElementById("coins").value
     const randomColumn = randomNumber()
     const randomLine = randomNumber()
-    historico.push(randomColumn, randomLine)
+    randomAnimal(randomLine, randomColumn)
 
-    console.log(e.target);
+    console.log(e.target)
     console.log(historico)
     console.log(randomColumn)
     console.log(randomLine)
     console.log(coins)
+    console.log(animal)
+    if (coins > totalCoins){
+        alert("Você não tem moedas o suficientes")
+    }
+    else{
+        win(randomColumn, randomLine, column, line, coins)
+    }
+    
 
-
-    win(randomColumn, randomLine, column, line, coins)    
 }
 
 betButton.addEventListener("click", betAction);
@@ -89,6 +111,16 @@ const win = (randomColumn, randomLine, column, line) => {
     }
 }
 
+const randomAnimal = (line, column) => {
+    console.log(line)
+    console.log(column)
+    const value = animais[line][column]
+    const div = document.createElement("li")
+    div.innerText = value
+    historicoContent.appendChild(div)
+    
+}
+
 const randomNumber = () => {
     return Math.floor(Math.random() * 4) + 0
 }
@@ -96,8 +128,8 @@ const randomNumber = () => {
 randomNumber();
 
 const showHistorico = () => {
-    historicoList.classList.toggle("show")
-    historicoList.classList.toggle("dontShow")
+    menu.classList.toggle("show")
+    menu.classList.toggle("dontShow")
     gameInterface.classList.toggle("dontShow")
     
 }
