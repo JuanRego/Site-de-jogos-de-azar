2611b121cce9e1e68278b4fe95b31d8a22f2bc76
Juan Rego <juanrego.s@outlook.com>
2021-11-25 14:27:35 -0300

    some corrections

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const callBicho = document.getElementById('call-jogo-do-bicho')
 const callPalavra = document.getElementById('call-caca-palavra')
-const body = document.getElementsByTagName('body')[0]
 
 const callingBicho = () =>{
     window.location = './jogo-do-bicho/jogo-do-bicho.html'
@@ -9,6 +8,6 @@ const callingBicho = () =>{
 callBicho.addEventListener('click', callingBicho)
 
 const callingPalavra = () =>{
-    window.location = './caca-palavra/caca-palavra.html'
+    window.location = './caca-palavras/index.html'
 }
 callPalavra.addEventListener('click', callingPalavra)
