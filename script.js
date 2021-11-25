3bc824bbbddf438d077343a657ef608c382ac570
Juan Rego <72818383+JuanRego@users.noreply.github.com>
2021-11-25 13:08:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,4 @@ callBicho.addEventListener('click', callingBicho)
 const callingPalavra = () =>{
     window.location = './caca-palavra/caca-palavra.html'
 }
-
 callPalavra.addEventListener('click', callingPalavra)
