fc5b7faf1df4856723054f17c9bb8b72896b94bf
kaique <kaique@kaiqs6669>
2021-11-25 03:20:16 -0300

    bug aleatoriedade consertado

--- caca-palavras/javascript.js
+++ caca-palavras/javascript.js
@@ -4,53 +4,58 @@ table.className = "tabela"
 let posicao = document.querySelector("td")
 body.appendChild(table)
 
-// array com palavras a serem achadas
+// array com palavras a serem achadas = 9 LETRAS CADA PALAVRA
 
 const palavrasUsadas = [
-    "SUSTENTARA", "CONFERIDOA", "ACONTECERA", "COMPACTARA", "INTIMIDARAb",
-    "MONITORIAA", "LIBERTINAA", "COLONIZARA", "ASTRONOMOA", "BINARISMOA",
-    "ESTAFANTEA", "DEGRADADOA", "INQUILINOA", "HIPOCRITAA", "IMPELIDOSA",
-    "ARRAIGADASA", "CONGRESSOA", "SOLITARIOA", "ETNOLOGIAA", "MUAMBEIROA",
+    "SUSTENTARRQWDFGHY", "CONFERIDORQWDFGHY", "ACONTECERRQWDFGHY", "COMPACTARRQWDFGHY", "INTIMIDARRQWDFGHY",
+    "MONITORIRQWDFGHY", "LIBERTINARQWDFGHY", "COLONIZARRQWDFGHY", "ASTRONOMORQWDFGHY", "BINARISMORQWDFGHY",
+    "ESTAFANTERQWDFGHY", "DEGRADADORQWDFGHY", "INQUILINORQWDFGHY", "HIPOCRITRQWDFGHY", "IMPELIDOSRQWDFGHY",
+    "ARRAIGADASRQWDFGHY", "CONGRESSORQWDFGHY", "SOLITARIORQWDFGHY", "ETNOLOGIRQWDFGHY", "MUAMBEIRORWDFGHY",
 ]
 
 //array com letras aleatórias
 
 const letrasAleatorias = [
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-]
+    "A",  "C", "E", "G", "I", "K", "M", "O", "Q", "S", "U", "W", "Y",'B', 'D', 'F', 'H', 'J', 'L', 'N', 'P', 'R', 'T', 'V', 'X', 'Z'
+    ]
 let arrayPalavras = []  
     
 // gerando aleatoriedade
 
-let p1 = Math.floor(Math.random() * 19)
-let p2 = Math.floor(Math.random() * 10)
-let p3 = Math.floor(Math.random() * 7)
-let p4 = Math.floor(Math.random()*  6)
+let p1 = Math.floor(Math.random() * 20)
+let p2 = Math.floor(Math.random() * 20)
+let p3 = Math.floor(Math.random() * 20)
+let p4 = Math.floor(Math.random()*  20)
+
+let p5 = Math.floor(Math.random() * 20)
 let letter1 = palavrasUsadas[p1].split('');   // variáveis quebrando em strings e gerando aleatoriedade
 let letter2 = palavrasUsadas[p2].split('');   // variáveis quebrando em strings
 let letter3 = palavrasUsadas[p3].split('');   // variáveis quebrando em strings
 let letter4 = palavrasUsadas[p4].split('');
 
-if (letter1.length < 12) {   //condição de coluna
-    letter1.push(letrasAleatorias[1])
+let letter5 = palavrasUsadas[p5]. split('');
+
+if (letter1.length <= 13) {   //condição de coluna
+    letter1.push(letrasAleatorias[0])
 }
-if (letter2.length < 12) {   //condição de coluna
+if (letter2.length <= 13) {   //condição de coluna
     letter2.push(letrasAleatorias[0])
 }
-if (letter3.length < 12) {  //condição de coluna
-    letter3.push(letrasAleatorias[7])
+if (letter3.length <= 13) {  //condição de coluna
+    letter3.push(letrasAleatorias[0])
 }
-if (letter4.length < 12){   //condição de coluna
-    letter4.push(letrasAleatorias[10])
+if (letter4.length <= 13){   //condição de coluna
+    letter4.push(letrasAleatorias[0])
+}
+if(letter5.length <= 13){
+    letter5.push(letrasAleatorias[0])
 }
 
 // condição de letras + colunas + linhas a serem inseridas. 
 
-for (let index = 0; index < 11; index++) {
+for (let index = 0; index <= 13; index++) {
     arrayPalavras[index] = []
-    for (let coluna = 0; coluna < 11; coluna++) {
+    for (let coluna = 0; coluna <= 13; coluna++) {
         arrayPalavras[index][coluna] = letrasAleatorias[coluna];
         if (index === 0) {
             arrayPalavras[index][coluna] = letter1[coluna] //Numero da coluna a ter a palavra
@@ -60,8 +65,10 @@ for (let index = 0; index < 11; index++) {
             arrayPalavras[index][coluna] = letter3[coluna]//Numero da coluna a ter a palavra
         } else if (index === 10){
             arrayPalavras[index][coluna] = letter4[coluna]//Numero da coluna a ter a palavra
-        }   else {
-            arrayPalavras[index][coluna] = letrasAleatorias[coluna + index];
+        } else if (index === 13) {
+            arrayPalavras[index][coluna] = letter5[coluna];
+        } else {
+            arrayPalavras[index][coluna] = letrasAleatorias[index + coluna]
         }
 
     }
@@ -70,12 +77,14 @@ for (let index = 0; index < 11; index++) {
 }
 resultado(arrayPalavras)
 
+// CRIANDO TD E TR EM HTML
+
 function resultado() {
     for (let i = 0; i < arrayPalavras.length; i++) {
         let tr = document.createElement("tr");
         for (let j = 0; j < arrayPalavras.length; j++) {
             let td = document.createElement("td");
-            td.className = "letra"
+            td.className = "letraAleatorias"
             let resultado = document.createTextNode(arrayPalavras[i][j])
             tr.appendChild(td)
             td.appendChild(resultado);
